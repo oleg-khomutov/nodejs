f802c7e8c6f8fe0efec334d7bdd59b47879a8118
xangie <oleg.khomutov@gmail.com>
2015-05-01 13:56:34 -0700

    Added DB update part

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/helloworld', function(req, res, next) {
-  res.render('helloworld', {title: 'Hello, World!'})
+  res.render('helloworld', {title: 'Hello, World!'});
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+
+    collection.find({}, {}, function(e, docs) {
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {title: 'Add New User'});
+});
+
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function(err, doc) {
+       if (err) {
+         res.send("There was a problem adding the information to the database.");
+       }
+       else {
+         res.location("userlist");
+         res.redirect("userlist");
+       }
+     }
+  )
 });
 
 module.exports = router;
