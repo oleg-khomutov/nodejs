966382a01d7ccd0578a5a319e77df48e4ad1f2ca
xangie <oleg.khomutov@gmail.com>
2015-05-02 20:15:14 -0700

    Added new functionality

--- TestProject2/app.js
+++ TestProject2/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongoskin');
+var db - mongo.db('mongodb://localhost:27017/TestProject2', {native_parser:true});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -15,12 +18,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(res, req, next){
+  req.db = db;
+  next();
+})
 
 app.use('/', routes);
 app.use('/users', users);
@@ -55,6 +62,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
