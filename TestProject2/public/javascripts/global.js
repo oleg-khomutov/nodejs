d1e0c86bec6cf6bf4bc7e14ec4f10ec1e4221e9f
xangie <oleg.khomutov@gmail.com>
2015-05-13 21:35:06 -0700

    Optimized rendering function

--- TestProject2/public/javascripts/global.js
+++ TestProject2/public/javascripts/global.js
@@ -8,7 +8,7 @@ function populateTable() {
   var tableContent = '';
 
   $.getJSON('/users/userlist', function(data) {
-    $.each(data, function(data) {
+    $.each(data, function() {
       tableContent += '<tr>';
       tableContent += '<td><a hred="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
       tableContent += '<td>' + this.email + '</td>';
