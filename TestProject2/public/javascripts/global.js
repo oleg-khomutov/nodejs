7b4e58be8d85852ab52b8abf15d29f9b8f9cc106
xangie <oleg.khomutov@gmail.com>
2015-05-13 21:56:27 -0700

    Added User Info events

--- TestProject2/public/javascripts/global.js
+++ TestProject2/public/javascripts/global.js
@@ -2,12 +2,15 @@ var userListData = [];
 
 $(document).ready(function() {
   populateTable();
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 function populateTable() {
   var tableContent = '';
 
   $.getJSON('/users/userlist', function(data) {
+    userListData = data;
+
     $.each(data, function() {
       tableContent += '<tr>';
       tableContent += '<td><a hred="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
@@ -20,3 +23,20 @@ function populateTable() {
     $('#userList table tbody').html(tableContent);
   });
 };
+
+function showUserInfo(event) {
+  event.preventDefault();
+
+  var thisUserName = $(this).attr('rel');
+
+  var arrayPosition = userListData
+     .map(function(arrayItem) {return arrayItem.username;})
+     .indexOf(thisUserName);
+
+  var thisUserObject = userListData[arrayPosition];
+
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+}
