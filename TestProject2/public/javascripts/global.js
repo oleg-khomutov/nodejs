25d28d18b2e2f34fc39ac8c4a16521fec4f6b7d0
xangie <oleg.khomutov@gmail.com>
2015-05-13 21:25:55 -0700

    fixed defect w/ user list

--- TestProject2/public/javascripts/global.js
+++ TestProject2/public/javascripts/global.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
   populateTable();
 });
 
-function populateTalbe() {
+function populateTable() {
   var tableContent = '';
 
   $.getJSON('/users/userlist', function(data) {
-    $.each(data, function(){
+    $.each(data, function(data) {
       tableContent += '<tr>';
-      tableContent += '<tr><a hred="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-      tableContent += '<tr>' + this.email + '</td>';
-      tableContent += '<tr><a hred="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+      tableContent += '<td><a hred="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td><a hred="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
       tableContent += '</tr>';
 
     });
-
+    alert(tableContent);
     $('#userList table tbody').html(tableContent);
   });
 };
