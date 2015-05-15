991c9a269bf7e8299fa525f98f7fb7d1fbf40ec6
xangie <oleg.khomutov@gmail.com>
2015-05-14 21:36:12 -0700

    Added new post route

--- TestProject2/routes/users.js
+++ TestProject2/routes/users.js
@@ -8,4 +8,11 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  db.collection('userlist').insert(req.body, function(err, result) {
+    res.send((err === null) ? { msg: '' } : { msd: err });
+  });
+});
+
 module.exports = router;
