598b633e31fbd70c7941f58e126d7ccd4e2fedd9
xangie <oleg.khomutov@gmail.com>
2015-05-14 22:05:18 -0700

    Added Add User function

--- TestProject2/public/javascripts/global.js
+++ TestProject2/public/javascripts/global.js
@@ -3,6 +3,7 @@ var userListData = [];
 $(document).ready(function() {
   populateTable();
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+  $('#btnAddUser').on('click', addUser);
 });
 
 function populateTable() {
@@ -39,4 +40,43 @@ function showUserInfo(event) {
   $('#userInfoAge').text(thisUserObject.age);
   $('#userInfoGender').text(thisUserObject.gender);
   $('#userInfoLocation').text(thisUserObject.location);
-}
+};
+
+function addUser(event) {
+  event.preventDefault();
+
+  var errorCount = 0;
+  $('#addUser input').each(function(index, val) {
+    if ($(this).val() === '') {errorCount++;}
+  });
+
+  if (errorCount === 0) {
+    var newUser = {
+      'username' : $('#addUser fieldset input#inputUserName').val(),
+      'email' : $('#addUser fieldset input#inputUserEmail').val(),
+      'fullname' : $('#addUser fieldset input#inputUserFullname').val(),
+      'age' : $('#addUser fieldset input#inputUserAge').val(),
+      'location' : $('#addUser fieldset input#inputUserLocation').val(),
+      'gender' : $('#addUser fieldset input#inputUserGender').val(),
+    }
+
+    $.ajax({
+      type: 'POST',
+      data: newUser,
+      url: '/users/adduser',
+      dataType: 'JSON'
+    }).done(function(response) {
+         if (response.msg === '') {
+           $('#addUser fieldset input').val('');
+           populateTable();
+         } 
+         else {
+           alert('Error: ' + response.msg);
+         }
+       });
+  }
+  else {
+    alert('Please fill in all fields');
+    return false;
+  }
+};
